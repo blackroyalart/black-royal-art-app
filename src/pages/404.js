720764191b527f097121b367074f37381c381ae6
Michelle Lam <mlam8428@gmail.com>
2020-11-10 19:19:16 -0500

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
 import Layout from "../layout/site-layout"
+import vectorArt from '../img/404_vector_art.png';
+import '../style/404.scss';
 
 const NotFoundPage = () => (
   <Layout>
-    Not found
+    <Container className="themed-container page-not-found" fluid="sm">
+      <Row>
+        <Col className="vertical-align">
+          <p className="not-found-text">
+            Page Not Found!
+          </p>
+        </Col>
+        <Col className="vertical-align">
+          <img src={vectorArt} className="about-me" alt="about me picture"/>
+        </Col>
+      </Row>
+
+    </Container>
   </Layout>
 )
 
