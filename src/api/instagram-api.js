c8ac491008b0c69121ea844cffb1962a38b0fd12
michelle <mlam8428@gmail.com>
2020-12-19 15:16:51 -0500

    Update instagram-api.js

--- src/api/instagram-api.js
+++ src/api/instagram-api.js
@@ -2,7 +2,7 @@ export const InstagramPosts = [
   {
     name: 'name',
     imageUrl: 'https://i.gyazo.com/c0f0d690052fd32a10a42b44769ce300.png',
-    postUrl: 'https://www.instagram.com/p/B-aFT19JRbn/',
+    postUrl: 'https://www.instagram.com/p/B5BG4cFANym/',
   },
   {
     name: 'name',
